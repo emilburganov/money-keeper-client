7c92baea8f865cf608e977dab1c7ce201343dc92
Emil Burganov <emil.burganov03@gmail.com>
2023-11-05 16:47:20 +0300

    fix: minor changes

--- src/components/Profile/ProfileCard.tsx
+++ src/components/Profile/ProfileCard.tsx
@@ -2,9 +2,20 @@ import Button from "@/components/UI/Button/Button";
 import Loader from "@/components/UI/Loader/Loader";
 import LoaderContext from "@/context/LoaderContext";
 import useStores from "@/hooks/useStores";
-import {Avatar, Box, Center, Flex, Heading, Stack, Text, useColorModeValue} from "@chakra-ui/react";
+import {
+    Avatar,
+    AvatarBadge,
+    Box,
+    Center,
+    Flex,
+    Heading,
+    Stack,
+    Text,
+    useColorModeValue,
+    useDisclosure,
+} from "@chakra-ui/react";
 import {observer} from "mobx-react-lite";
-import {FC, useContext, useEffect} from "react";
+import {FC, useContext, useEffect, useRef, useState} from "react";
 import {useTranslation} from "react-i18next";
 import {useNavigate} from "react-router-dom";
 
@@ -13,6 +24,27 @@ const ProfileCard: FC = observer(() => {
     const {authStore} = useStores();
     const navigate = useNavigate();
     const {isLoading, setLoading} = useContext(LoaderContext);
+
+    const [userProfile, setUserProfile] = useState(null);
+    const {onOpen} = useDisclosure();
+    const profileImage = useRef(null);
+
+    const openChooseImage = () => {
+        profileImage.current.click();
+    };
+
+    const changeProfileImage = event => {
+        const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/jpg"];
+        const selected = event.target.files[0];
+
+        if (selected && ALLOWED_TYPES.includes(selected.type)) {
+            let reader = new FileReader();
+            reader.onloadend = () => setUserProfile(reader.result);
+            return reader.readAsDataURL(selected);
+        }
+
+        onOpen();
+    };
 
     const logout = async () => {
         await authStore.logout();
@@ -48,7 +80,19 @@ const ProfileCard: FC = observer(() => {
                     background={"green.500"}
                 />
                 <Flex justify={"center"} mt={-12}>
-                    <Avatar size={"xl"}/>
+                    <Avatar
+                        size="xl"
+                        name={authStore.user.name}
+                        cursor="pointer"
+                        onClick={openChooseImage}
+                        src={userProfile ? userProfile : "/img/tim-cook.jpg"}
+                    />
+                    <input
+                        hidden
+                        type="file"
+                        ref={profileImage}
+                        onChange={changeProfileImage}
+                    />
                 </Flex>
                 <Box p={6}>
                     <Stack spacing={2} align={"center"}>
