12f3763838c9009101beccee608c38cec84e95d1
emilburganov <emil.burganov03@gmail.com>
2024-05-26 20:21:27 +0300

    fix: changed router to hash

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,27 @@
-import { Routing } from "@/pages";
+import {Routing} from "@/pages";
 import "@/shared/i18n";
-import { Spinner } from "@/shared/ui/(spinner)/spinner";
-import { Footer, Header } from "@/widgets/(includes)";
-import { createStandaloneToast } from "@chakra-ui/react";
-import { Suspense } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Providers } from "./providers";
+import {Spinner} from "@/shared/ui/(spinner)/spinner";
+import {Footer, Header} from "@/widgets/(includes)";
+import {createStandaloneToast} from "@chakra-ui/react";
+import {Suspense} from "react";
+import {HashRouter} from "react-router-dom";
+import {Providers} from "./providers";
 
-const { ToastContainer } = createStandaloneToast();
+const {ToastContainer} = createStandaloneToast();
 
 const App = () => {
-  return (
-    <Providers>
-      <ToastContainer />
-      <BrowserRouter>
-        <Header />
-        <Suspense fallback={<Spinner />}>
-          <Routing />
-        </Suspense>
-        <Footer />
-      </BrowserRouter>
-    </Providers>
-  );
+    return (
+        <Providers>
+            <ToastContainer />
+            <HashRouter>
+                <Header />
+                <Suspense fallback={<Spinner />}>
+                    <Routing />
+                </Suspense>
+                <Footer />
+            </HashRouter>
+        </Providers>
+    );
 };
 
 export default App;
