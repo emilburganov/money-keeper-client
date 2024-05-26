3ad268d8901ddb9278cda2ddae1d1d988f7b1353
emilburganov <emil.burganov03@gmail.com>
2024-05-27 00:17:48 +0300

    fix: api port changed

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://185.84.163.156/";
+export const BACKEND_URL = "http://185.84.163.156:81/";
 export const API_URL = BACKEND_URL + "api";
 export { ROUTE_CONSTANTS } from "./routes";
